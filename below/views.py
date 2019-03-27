5fe3e394c96f6e018f9d4a5f74c8dbba70edc57b
Geerocktricks <geerockface4@gmail.com>
2019-03-27 21:38:19 +0300

    This commit creates the hero div

--- below/views.py
+++ below/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def home(request):
+    '''
+    view function that displays the home page
+    '''
+    return render(request , 'index.html')
